39832dd5b0c7e7995d2e05901d886709d92765b9
John V. Petersen <johnvpetersen@gmail.com>
2019-07-13 18:23:07 -0400

    Initial commit

---
+++ PDFLibrary/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+
+namespace PDFLibrary
+{
+    public class Main
+    {
+
+        public IDictionary<string, PdfFormField> GetData(string file)
+        {
+
+            IDictionary<string, PdfFormField> fields;
+
+            using (var reader = new PdfReader(file))
+            {
+                using (var doc = new PdfDocument(reader))
+                {
+                    fields = PdfAcroForm.GetAcroForm(doc,false).GetFormFields();
+                }
+            }
+
+            return fields;
+        }
+    }
+}
---
+++ PDFLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PDFLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PDFLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("293c2d7e-5da0-4d9a-bd3f-22396578812c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
