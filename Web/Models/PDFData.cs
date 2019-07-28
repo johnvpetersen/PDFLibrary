10a5188ac31eaee3957f12ee6edbc2b1919ab786
John V. Petersen <johnvpetersen@gmail.com>
2019-07-28 09:27:41 -0400

    Added jquery plugins and adjusted UI

---
+++ Web/Models/PDFData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class PDFData
+    {
+      [DisplayName("First")]  public string FirstName { get; set; }
+      [DisplayName("Middle")] public string MiddleInitial { get; set; }
+      [DisplayName("Last")] public string LastName { get; set; }
+      [DisplayName("Street")] public string Street { get; set; }
+      [DisplayName("City")] public string City { get; set; }
+      [DisplayName("State")] public string State { get; set; }
+      [DisplayName("Zip")] public string Zip { get; set; }
+      [DisplayName("Customer Since")] public DateTime CustomerSince { get; set; }
+      [DisplayName("Active")] public bool Active { get; set; }
+      [DisplayName("Point Balance")] public int PointBalance { get; set; }
+      [DisplayName("TIN")] public string TIN { get; set; }
+
+
+
+    }
+}
