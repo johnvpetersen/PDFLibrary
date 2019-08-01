150723c864b1bcd4b7c493ae6047f1b9085c8aeb
John V. Petersen <johnvpetersen@gmail.com>
2019-07-31 21:13:17 -0400

    Updte

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+
+    
+
+
         public ActionResult Index()
         {
             return View();
@@ -31,13 +36,92 @@ namespace Web.Controllers
 
         public ActionResult CreatePDF()
         {
-            return View();
+
+
+
+            string state = null;
+            if (TempData["States"] != null)
+                state = ((PDFData) TempData["States"]).State;
+
+            TempData["States"] = getStates(state);
+
+            return TempData["Model"] != null ? View(TempData["Model"]) : View();
+
+
         }
 
         [HttpPost]
         public ActionResult CreatePDF(PDFData pdfData)
         {
+            TempData["Model"] = pdfData;
+
             return RedirectToAction("CreatePDF");
         }
+
+
+        List<SelectListItem> getStates(string state = null)
+        {
+            var states =   new List<SelectListItem>()
+    {
+        new SelectListItem() {Text="Alabama", Value="AL"},
+        new SelectListItem() { Text="Alaska", Value="AK"},
+        new SelectListItem() { Text="Arizona", Value="AZ"},
+        new SelectListItem() { Text="Arkansas", Value="AR"},
+        new SelectListItem() { Text="California", Value="CA"},
+        new SelectListItem() { Text="Colorado", Value="CO"},
+        new SelectListItem() { Text="Connecticut", Value="CT"},
+        new SelectListItem() { Text="District of Columbia", Value="DC"},
+        new SelectListItem() { Text="Delaware", Value="DE"},
+        new SelectListItem() { Text="Florida", Value="FL"},
+        new SelectListItem() { Text="Georgia", Value="GA"},
+        new SelectListItem() { Text="Hawaii", Value="HI"},
+        new SelectListItem() { Text="Idaho", Value="ID"},
+        new SelectListItem() { Text="Illinois", Value="IL"},
+        new SelectListItem() { Text="Indiana", Value="IN"},
+        new SelectListItem() { Text="Iowa", Value="IA"},
+        new SelectListItem() { Text="Kansas", Value="KS"},
+        new SelectListItem() { Text="Kentucky", Value="KY"},
+        new SelectListItem() { Text="Louisiana", Value="LA"},
+        new SelectListItem() { Text="Maine", Value="ME"},
+        new SelectListItem() { Text="Maryland", Value="MD"},
+        new SelectListItem() { Text="Massachusetts", Value="MA"},
+        new SelectListItem() { Text="Michigan", Value="MI"},
+        new SelectListItem() { Text="Minnesota", Value="MN"},
+        new SelectListItem() { Text="Mississippi", Value="MS"},
+        new SelectListItem() { Text="Missouri", Value="MO"},
+        new SelectListItem() { Text="Montana", Value="MT"},
+        new SelectListItem() { Text="Nebraska", Value="NE"},
+        new SelectListItem() { Text="Nevada", Value="NV"},
+        new SelectListItem() { Text="New Hampshire", Value="NH"},
+        new SelectListItem() { Text="New Jersey", Value="NJ"},
+        new SelectListItem() { Text="New Mexico", Value="NM"},
+        new SelectListItem() { Text="New York", Value="NY"},
+        new SelectListItem() { Text="North Carolina", Value="NC"},
+        new SelectListItem() { Text="North Dakota", Value="ND"},
+        new SelectListItem() { Text="Ohio", Value="OH"},
+        new SelectListItem() { Text="Oklahoma", Value="OK"},
+        new SelectListItem() { Text="Oregon", Value="OR"},
+        new SelectListItem() { Text="Pennsylvania", Value="PA"},
+        new SelectListItem() { Text="Rhode Island", Value="RI"},
+        new SelectListItem() { Text="South Carolina", Value="SC"},
+        new SelectListItem() { Text="South Dakota", Value="SD"},
+        new SelectListItem() { Text="Tennessee", Value="TN"},
+        new SelectListItem() { Text="Texas", Value="TX"},
+        new SelectListItem() { Text="Utah", Value="UT"},
+        new SelectListItem() { Text="Vermont", Value="VT"},
+        new SelectListItem() { Text="Virginia", Value="VA"},
+        new SelectListItem() { Text="Washington", Value="WA"},
+        new SelectListItem() { Text="West Virginia", Value="WV"},
+        new SelectListItem() { Text="Wisconsin", Value="WI"},
+        new SelectListItem() { Text="Wyoming", Value="WY"}
+    };
+            if (state != null)
+               states.First(x => x.Value == state).Selected = true;
+
+            return states;
+
+        }
+
+
     }
 }
--- Web/Models/PDFData.cs
+++ Web/Models/PDFData.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Web.Models
 {
     public class PDFData
     {
+        private string _tin = string.Empty;
+        private string _customerSince = string.Empty;
+        private string _pointBalance = string.Empty;
+        private string _tinFormatted = string.Empty;
+        private string _customerSinceFormatted = string.Empty;
+        private string _pointBalanceFormatted = string.Empty;
+
+
       [DisplayName("First")]  public string FirstName { get; set; }
       [DisplayName("Middle")] public string MiddleInitial { get; set; }
       [DisplayName("Last")] public string LastName { get; set; }
@@ -15,12 +25,54 @@ namespace Web.Models
       [DisplayName("City")] public string City { get; set; }
       [DisplayName("State")] public string State { get; set; }
       [DisplayName("Zip")] public string Zip { get; set; }
-      [DisplayName("Customer Since")] public DateTime CustomerSince { get; set; }
+      [DisplayName("Customer Since")] public string CustomerSince { get => _customerSince; set => _customerSince = processCustomerSince(value); }
       [DisplayName("Active")] public bool Active { get; set; }
-      [DisplayName("Point Balance")] public int PointBalance { get; set; }
-      [DisplayName("TIN")] public string TIN { get; set; }
+      [DisplayName("Point Balance")] public string PointBalance { get => _pointBalance ;   set => _pointBalance = processPointBalance(value);}
+      [DisplayName("TIN")] public string TIN { get => _tin; set => _tin = processTIN(value); }
+
+
+      string processPointBalance(string pointBalance)
+      {
+          int result;
 
 
 
+
+
+          if (string.IsNullOrEmpty(pointBalance) ||  !int.TryParse(pointBalance.Replace(",", ""), out result))
+              pointBalance = string.Empty;
+
+
+          _pointBalanceFormatted = pointBalance;
+
+            _pointBalance = pointBalance.Replace(",", "");
+
+          return pointBalance.Replace(",","");
+      }
+      string processCustomerSince(string customerSince)
+      {
+          DateTime result;
+          if (!DateTime.TryParse(customerSince, out result))
+              customerSince = string.Empty;
+
+          _customerSinceFormatted = customerSince;
+
+          return customerSince.Replace("/","");
+      }
+      string processTIN(string tin)
+      {
+
+          if (!Regex.Match(tin.Replace("-",""), @"^[0-9]{9}$").Success)
+              tin = string.Empty;
+
+
+          _tinFormatted = tin;
+
+            tin =  tin.Replace("-", "");
+            
+
+
+          return tin;
+      }
     }
 }
